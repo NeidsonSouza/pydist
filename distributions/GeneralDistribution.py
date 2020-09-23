c248cdc90f3fdbb6eec18f46a709de7b00880c23
Neidson dos Santos Souza <neidson@seg.eti.br>
2020-09-23 11:03:39 -0300

    Modification Distribution class

--- distributions/GeneralDistribution.py
+++ distributions/GeneralDistribution.py
@@ -5,7 +5,7 @@ class Distribution:
     Attributes:
         mean (float): mean value of the distribution
         standard_deviation (float): standard_deviation of the distribution
-        data_list (list of floats): a list of floats extracted from the
+        data (list of floats): a list of floats extracted from the
         data file.
     """
 
@@ -14,13 +14,13 @@ class Distribution:
 
         self.mean = mean
         self.standard_deviation = standard_deviation
-        self.data_list = []
+        self.data = []
     
 
     def read_data_file(self, file_name):
         """ Function to read in data from a txt file. The txt file shoud have
         one number (zero or one) per line. The numbers are stored in the
-        'data_list' atrribute.
+        'data' atrribute.
 
         Args:
             file_name (string): name of a file to read from
@@ -37,17 +37,8 @@ class Distribution:
                 file_data.append(float(line))
                 line = file.readline()
             
-        file.close()
+            file.close()
 
-        if not has_list_only_zeros_and_ones(file_data):
-            raise ValueError('each item in txt file should be zero or one')
+        self.data = file_data
 
-        self.data_list = ['%.1f' % item for item in file_data]
-    
-
-    @staticmethod
-    def has_list_only_zeros_and_ones(any_list):
-        """ Function to check if each item of a list is zero or one """
-
-        list_result = [item == 0 or item == 1 for item in any_list]
-        return all(list_result)
+        return self.data
